e5031a2663732f33269af43693affbb6d726fe3e
Jacob Paulin <jacobwpaulin@gmail.com>
2023-08-13 22:39:38 -0400

    Remove useless credit

--- src/main/java/acmecollege/rest/resource/StudentResource.java
+++ src/main/java/acmecollege/rest/resource/StudentResource.java
@@ -7,9 +7,6 @@
  * 
  * Updated by:  Group 40
  *   41024610, Jacob, Paulin
- *   041053188, Taeung, Park 
- *   041065803, Doyoung, Kim 
- *   041053986, Dawon, Jun 
  */
 package acmecollege.rest.resource;
 
